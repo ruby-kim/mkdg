cafbe3c0d10943b13c8f5f1324521893d2620f43
kimkyeongnam <kkyy0126@naver.com>
2020-04-16 09:40:13 +0900

    [New] main.py
    패키지를 실행시키는 전체적인 코드 작성

---
+++ main.py
@@ -0,0 +1,13 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Implement making misspelled korean data operator
+"""
+
+from mkdg.option import setting_parse
+
+if __name__ == '__main__':
+    """ Setting parameters input & description """
+    filename = setting_parse()
+    print(filename)
