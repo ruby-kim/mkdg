00f71e6cc9a333238c4fb1ada785fa59eab272e7
kimkyeongnam <kkyy0126@naver.com>
2020-04-27 14:02:29 +0900

    Make code to class

--- mkdg/utils/frequency.py
+++ mkdg/utils/frequency.py
@@ -13,32 +13,29 @@ def tag_switch(tag):
 
     Args:
         :param: tag(str)
-
     Returns:
         :param: tag name(str)
     """
-    return{
-        "VA" or "VCN" or "VCP": "adjective",
-        "MAG": "adverb",
-        "MAJ": "conjunction",
-        "MM": "determiner",
-        "EC" or "EF" or "ETM" or "ETN": "eomi",
-        "JC" or "JKC" or "JKG" or "JKV" or "JKB" or "JKO" or "JKQ" or "JKS" or "JS": "josa",
-        "NNG" or "NNB" or "NNP" or "NP" or "NR": "noun",
-        "EP": "preEomi",
-        "XPN" or "XSA" or "XSN" or "XSV": "suffix",
-        "VV" or "VX": "verb"
+    return {
+        "VA" or "VCN" or "VCP": "adjective",    # 형용사
+        "MAG": "adverb",    # 부사
+        "MAJ": "conjunction",   # 접속사
+        "MM": "determiner", # 관형사
+        "EC" or "EF" or "ETM" or "ETN": "eomi", # 어미
+        "JC" or "JKC" or "JKG" or "JKV" or "JKB" or "JKO" or "JKQ" or "JKS" or "JS": "josa",    # 조사
+        "NNG" or "NNB" or "NNP" or "NP" or "NR": "noun",    # 명사
+        "EP": "preEomi",    # 선어말어미
+        "XPN" or "XSA" or "XSN" or "XSV": "suffix", # 접사
+        "VV" or "VX": "verb"    # 동사
     }.get(tag, -1)
 
 
 def tag_cnt(word, tag_dict):
     """
     check word existence in tag_dict & count word
-
     Args:
         :param: word(str)
         :param: tag_dict(dictionary)
-
     Returns:
         :param: tag_dict(dictionary)
     """
@@ -48,93 +45,108 @@ def tag_cnt(word, tag_dict):
     return tag_dict
 
 
+class Tag_dict:
+    def __init__(self, content):
+        self.content = content
+        self.komoran = Komoran(userdic='./data/user_dic.txt')
+        self.adjective_dict = dict()  # 형용사: VA, VCN, VCP
+        self.adverb_dict = dict()  # 부사: MAG
+        self.conjunction_dict = dict()  # 접속사: MAJ
+        self.determiner_dict = dict()  # 관형사: MM
+        self.eomi_dict = dict()  # 어미: EC, EF, ETM, ETN
+        self.josa_dict = dict()  # 조사: JC, JKC, JKG, JKV, JKB, JKO, JKQ, JKS, JX
+        self.noun_dict = dict()  # 명사: NNG, NNB, NNP, NP, NR
+        self.preEomi_dict = dict()  # 선어말어미: EP
+        self.suffix_dict = dict()  # 접사: XPN, XSA, XSN, XSV
+        self.verb_dict = dict()  # 동사: VV, VX
+
+    def print_frequency(self, cnt=30):
+        """
+            print dict values frequency (descending)
+
+            Args:
+                :param: cnt(int)
+            Returns:
+                :param: tagDict(1st ~ until cnt-th) (dict)
+        """
+        self.adjective_dict = sorted(self.adjective_dict.items(), key=lambda x: x[1], reverse=True)
+        self.adverb_dict = sorted(self.adverb_dict.items(), key=lambda x: x[1], reverse=True)
+        self.conjunction_dict = sorted(self.conjunction_dict.items(), key=lambda x: x[1], reverse=True)
+        self.determiner_dict = sorted(self.determiner_dict.items(), key=lambda x: x[1], reverse=True)
+        self.eomi_dict = sorted(self.eomi_dict.items(), key=lambda x: x[1], reverse=True)
+        self.josa_dict = sorted(self.josa_dict.items(), key=lambda x: x[1], reverse=True)
+        self.noun_dict = sorted(self.noun_dict.items(), key=lambda x: x[1], reverse=True)
+        self.preEomi_dict = sorted(self.preEomi_dict.items(), key=lambda x: x[1], reverse=True)
+        self.suffix_dict = sorted(self.suffix_dict.items(), key=lambda x: x[1], reverse=True)
+        self.verb_dict = sorted(self.verb_dict.items(), key=lambda x: x[1], reverse=True)
+
+        print("형용사:")
+        print(self.adjective_dict[:cnt])
+        print("\n부사:")
+        print(self.adverb_dict[:cnt])
+        print("\n접속사:")
+        print(self.conjunction_dict[:cnt])
+        print("\n관형사:")
+        print(self.determiner_dict[:cnt])
+        print("\n어미:")
+        print(self.eomi_dict[:cnt])
+        print("\n조사:")
+        print(self.josa_dict[:cnt])
+        print("\n명사:")
+        print(self.noun_dict[:cnt])
+        print("\n선어말어미:")
+        print(self.preEomi_dict[:cnt])
+        print("\n접사:")
+        print(self.suffix_dict[:cnt])
+        print("\n동사:")
+        print(self.verb_dict[:cnt])
+
+    def judge_tag(self):
+        for text in self.content:
+            posList = self.komoran.pos(text)
+            for pos in posList:
+                # preprocessing
+                word = re.sub("[ㄱ-ㅎ|ㅏ-ㅣ|.,?!]", repl="", string=str(pos[0]))
+                if word == "":
+                    continue
+
+                # seperate tag & count
+                tagName = tag_switch(pos[1])
+                if tagName != -1:
+                    if tagName == "adjective":
+                        self.adjective_dict = tag_cnt(word, self.adjective_dict)
+                    elif tagName == "adverb":
+                        self.adverb_dict = tag_cnt(word, self.adverb_dict)
+                    elif tagName == "conjunction":
+                        self.conjunction_dict = tag_cnt(word, self.conjunction_dict)
+                    elif tagName == "determiner":
+                        self.determiner_dict = tag_cnt(word, self.determiner_dict)
+                    elif tagName == "eomi":
+                        self.eomi_dict = tag_cnt(word, self.eomi_dict)
+                    elif tagName == "josa":
+                        self.josa_dict = tag_cnt(word, self.josa_dict)
+                    elif tagName == "noun":
+                        self.noun_dict = tag_cnt(word, self.noun_dict)
+                    elif tagName == "preEomi":
+                        self.preEomi_dict = tag_cnt(word, self.preEomi_dict)
+                    elif tagName == "suffix":
+                        self.suffix_dict = tag_cnt(word, self.suffix_dict)
+                    elif tagName == "verb":
+                        self.verb_dict = tag_cnt(word, self.verb_dict)
+
+
 def frequency(contents):
     """
     check word frequency
 
     Args:
         :param: contents(list)
-
     Returns:
         :param: word_dict(dict)
     """
-    komoran = Komoran(userdic='./data/user_dic.txt')
-
-    adjective_dict = dict()     # 형용사: VA, VCN, VCP
-    adverb_dict = dict()        # 부사: MAG
-    conjunction_dict = dict()   # 접속사: MAJ
-    determiner_dict = dict()    # 관형사: MM
-    eomi_dict = dict()          # 어미: EC, EF, ETM, ETN
-    josa_dict = dict()          # 조사: JC, JKC, JKG, JKV, JKB, JKO, JKQ, JKS, JX
-    noun_dict = dict()          # 명사: NNG, NNB, NNP, NP, NR
-    preEomi_dict = dict()       # 선어말어미: EP
-    suffix_dict = dict()        # 접사: XPN, XSA, XSN, XSV
-    verb_dict = dict()          # 동사: VV, VX
-
-    for text in contents:
-        posList = komoran.pos(text)
-        for pos in posList:
-            # preprocessing
-            word = re.sub("[ㄱ-ㅎ|ㅏ-ㅣ|.,?!]", repl="", string=str(pos[0]))
-            if word == "":
-                continue
-
-            # seperate tag & count
-            tagName = tag_switch(pos[1])
-            if tagName != -1:
-                if tagName == "adjective":
-                    adjective_dict = tag_cnt(word, adjective_dict)
-                elif tagName == "adverb":
-                    adverb_dict = tag_cnt(word, adverb_dict)
-                elif tagName == "conjunction":
-                    conjunction_dict = tag_cnt(word, conjunction_dict)
-                elif tagName == "determiner":
-                    determiner_dict = tag_cnt(word, determiner_dict)
-                elif tagName == "eomi":
-                    eomi_dict = tag_cnt(word, eomi_dict)
-                elif tagName == "josa":
-                    josa_dict = tag_cnt(word, josa_dict)
-                elif tagName == "noun":
-                    noun_dict = tag_cnt(word, noun_dict)
-                elif tagName == "preEomi":
-                    preEomi_dict = tag_cnt(word, preEomi_dict)
-                elif tagName == "suffix":
-                    suffix_dict = tag_cnt(word, suffix_dict)
-                elif tagName == "verb":
-                    verb_dict = tag_cnt(word, verb_dict)
-
-    # sorting by cnt
-    adjective_dict = sorted(adjective_dict.items(), key=lambda x: x[1], reverse=True)
-    adverb_dict = sorted(adverb_dict.items(), key=lambda x: x[1], reverse=True)
-    conjunction_dict = sorted(conjunction_dict.items(), key=lambda x: x[1], reverse=True)
-    determiner_dict = sorted(determiner_dict.items(), key=lambda x: x[1], reverse=True)
-    eomi_dict = sorted(eomi_dict.items(), key=lambda x: x[1], reverse=True)
-    josa_dict = sorted(josa_dict.items(), key=lambda x: x[1], reverse=True)
-    noun_dict = sorted(noun_dict.items(), key=lambda x: x[1], reverse=True)
-    preEomi_dict = sorted(preEomi_dict.items(), key=lambda x: x[1], reverse=True)
-    suffix_dict = sorted(suffix_dict.items(), key=lambda x: x[1], reverse=True)
-    verb_dict = sorted(verb_dict.items(), key=lambda x: x[1], reverse=True)
-
-    print("형용사:")
-    print(adjective_dict[:30])
-    print("\n부사:")
-    print(adverb_dict[:30])
-    print("\n접속사:")
-    print(conjunction_dict[:30])
-    print("\n관형사:")
-    print(determiner_dict[:30])
-    print("\n어미:")
-    print(eomi_dict[:30])
-    print("\n조사:")
-    print(josa_dict[:30])
-    print("\n명사:")
-    print(noun_dict[:30])
-    print("\n선어말어미:")
-    print(preEomi_dict[:30])
-    print("\n접사:")
-    print(suffix_dict[:30])
-    print("\n동사:")
-    print(verb_dict[:30])
+    dict = Tag_dict(contents)
+    dict.judge_tag()
+    dict.print_frequency()
 
 
 if __name__ == "__main__":
@@ -142,4 +154,3 @@ if __name__ == "__main__":
     path = "./data/" + filename
     raw_text = read_text_file(path)
     frequency(raw_text)
-
