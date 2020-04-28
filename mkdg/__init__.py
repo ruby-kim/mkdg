9c38b136db9e67ce2c36f1bff397259e2193103d
kimkyeongnam <kkyy0126@naver.com>
2020-04-28 14:17:05 +0900

    Modify settings

--- mkdg/__init__.py
+++ mkdg/__init__.py
@@ -4,8 +4,8 @@
 __name__ = 'mkdg'
 __description__ = 'Misspelled Korean Data Generator'
 __version__ = '0.0.0'
-__url__ = 'https://github.com/study-artificial-intelligence/mkdg'
-__download_url__ = 'https://github.com/study-artificial-intelligence/mkdg'
+__url__ = 'https://github.com/study-ai-data/mkdg'
+__download_url__ = 'https://github.com/study-ai-data/mkdg'
 __install_requires__ = [
     "konlpy",   # komoran
 ]
