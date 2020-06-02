0d75f44770444a7bb2af080da72f8ddffd0bd80f
kimkyeongnam <kkyy0126@naver.com>
2020-06-02 16:11:58 +0900

    Change user dict arrange code strategy

--- mkdg/utils/arrange.py
+++ mkdg/utils/arrange.py
@@ -8,36 +8,23 @@ def arrangement_text():
     data = read_text_file("data/user_dic.txt")
     mydict = dict()
 
-    for i in range(len(data)):
-        """ delete overlap word """
-        unique_value = ""
-        for j in data[i]:
-            if j not in unique_value:
-                unique_value += j
-        """ make dictionary key, value """
-        flag = False
-        if " " in unique_value:
-            tmp = unique_value.split(" ")
-            flag = True
-        elif "\t" in unique_value:
-            tmp = unique_value.split("\t")
-            flag = True
-        else:
-            tmp = unique_value
-
-        if flag is True:
+    """ make dictionary key, value """
+    for elem in data:
+        elem = elem.replace("\n", "")
+        if "\t" in elem:
+            tmp = elem.split("\t")
             mydict[tmp[0]] = tmp[1]
         else:
-            mydict[tmp] = ""
+            mydict[elem] = ""
     mydict = sorted(mydict.items())
 
     """ make dictionary to list """
     result = list()
     for elem in mydict:
         if elem[1] is "":
-            result.append(elem[0])
+            result.append(elem[0]+"\n")
         else:
-            result.append(elem[0]+"\t"+elem[1])
+            result.append(elem[0]+"\t"+elem[1]+"\n")
     return result
 
 
