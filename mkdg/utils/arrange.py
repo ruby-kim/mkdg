3af083e58b73ce2ebf496ad49d42e13f165a985b
kimkyeongnam <kkyy0126@naver.com>
2020-06-09 13:50:27 +0900

    Completed making code: analyze, arrange

--- mkdg/utils/arrange.py
+++ mkdg/utils/arrange.py
@@ -1,11 +1,11 @@
 import os
 
-from mkdg.utils.loadfile import read_text_file
+from mkdg.utils.loadfile import read_text_file, read_xlsx_file
 from mkdg.utils.writefile import save_text_file
 
 
 def arrangement_text():
-    data = read_text_file("data/user_dic.txt")
+    data = read_text_file("user_dic.txt")
     mydict = dict()
 
     """ make dictionary key, value """
@@ -21,19 +21,34 @@ def arrangement_text():
     """ make dictionary to list """
     result = list()
     for elem in mydict:
-        if elem[1] is "":
+        if elem[1] == "":
             result.append(elem[0]+"\n")
         else:
             result.append(elem[0]+"\t"+elem[1]+"\n")
     return result
 
 
-def check_null(data):
-    for i in range(len(data)):
-        print(i, data[i], end="")
+def arrange_xlsx():
+    data = read_xlsx_file("noun_standard")
+    print(data)
+
+
+def check_text_null():
+    data = read_text_file("user_dic.txt")
+    for text in data:
+        text = text.replace("\n", "")
+        if '\t' in text:
+            text = text.replace('\t', '<tab>')
+        print(text)
 
 
 if __name__ == "__main__":
+    """ check null text """
+    # check_text_null()
+
+    """ arrange user_dict.txt & save result """
     result = arrangement_text()
-    # check_null(result)
-    save_text_file(os.getcwd() + "/data/user_dic.txt", result, "user_dic")
+    save_text_file(os.getcwd() + "/user_dic.txt", result, "user_dic")
+
+    """ arrange xlsx & save result """
+    # arrange_xlsx()
